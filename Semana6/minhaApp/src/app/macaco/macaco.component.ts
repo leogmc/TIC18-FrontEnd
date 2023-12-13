3d5936a27b7f13f742d33e017d96c6e295a75b9d
Leandro <ifbaianoleo@gmail.com>
2023-12-13 17:55:48 -0300

    Enviando semana6

---
+++ Semana6/minhaApp/src/app/macaco/macaco.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-macaco',
+  templateUrl: './macaco.component.html',
+  styleUrl: './macaco.component.css'
+})
+export class MacacoComponent {
+
+}
