920712bf7715d9ce27c23d2d8385b1ad9196e95f
Leandro <ifbaianoleo@gmail.com>
2024-02-21 17:07:15 -0300

    Enviando exercicio de formulario com routes

---
+++ S12/Exercises/residencia/src/app/form-adicionar/form-adicionar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-adicionar',
+  templateUrl: './form-adicionar.component.html',
+  styleUrl: './form-adicionar.component.css'
+})
+export class FormAdicionarComponent {
+
+}
