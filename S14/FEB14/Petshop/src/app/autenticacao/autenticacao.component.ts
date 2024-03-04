ce2b09c7a96b59f4f18e9328cc0efb7078988ec8
Leandro <ifbaianoleo@gmail.com>
2024-03-04 17:15:51 -0300

    Iniciando FEB14

---
+++ S14/FEB14/Petshop/src/app/autenticacao/autenticacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autenticacao',
+  templateUrl: './autenticacao.component.html',
+  styleUrl: './autenticacao.component.css'
+})
+export class AutenticacaoComponent {
+
+}
