713e80e01263c7491b7c3765daefe9bf410b2b69
Leandro <ifbaianoleo@gmail.com>
2024-03-05 14:42:49 -0300

    Subindo FEB-14

--- S14/FEB14/Petshop/src/app/autenticacao/autenticacao.component.ts
+++ S14/FEB14/Petshop/src/app/autenticacao/autenticacao.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AutenticaService } from '../autentica.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-autenticacao',
@@ -23,56 +24,59 @@ export class AutenticacaoComponent {
 
   }
 
-  onSubmit(formulario: NgForm){
+  onSubmit(formulario: NgForm) {
     console.log("OnSubmit iniciado");
-    if(!formulario.valid){
+    
+    if (!formulario.valid) {
       console.log("Formulário inválido.");
       return;
     }
+  
     const email = formulario.value.inputEmail;
     const password = formulario.value.inputPassword;
   
     this.estaCarregando = true;
   
-    if(this.modoLogin){
-      this.authService.loginUser(email, password).subscribe(
-        responseData => {
-          console.log(responseData);
-          this.estaCarregando = false;
-          this.temErro = false;
-          this.router.navigate(['/cadastrarAtendimento']);
-        }
-      );
+    let authObservable: Observable<any>;
+  
+    if (this.modoLogin) {
+      authObservable = this.authService.loginUser(email, password);
     } else {
-      this.authService.signupUser(email, password).subscribe(
-        responseData => {
-          console.log(responseData);
-          this.estaCarregando = false;
-          this.temErro = false;
-          this.router.navigate(['/cadastrarAtendimento']);
-        },
-        error => {
-          console.log(error);
-          if (error.error && error.error.error && error.error.error.message) {
-            switch (error.error.error.message) {
-              case 'EMAIL_EXISTS':
-                this.erro = 'O e-mail já está em uso.';
-                break;
-              case 'INVALID_PASSWORD':
-                this.erro = 'A senha inserida é inválida.';
-                break;
-              default:
-                this.erro = 'Ocorreu um erro ao cadastrar o usuário.';
-                break;
-            }
-            this.temErro = true;
-            this.estaCarregando = false;
-          }
-        }
-      );
+      authObservable = this.authService.signupUser(email, password);
     }
   
+    authObservable.subscribe({
+      next: responseData => {
+        console.log(responseData);
+        this.estaCarregando = false;
+        this.temErro = false;
+        this.router.navigate(['/cadastrarAtendimento']);
+      },
+      error: error => {
+        console.log(error);
+        this.handleAuthError(error);
+      }
+    });
+  
     formulario.reset();
+  }
+  
+  private handleAuthError(error: any) {
+    if (error.error && error.error.error && error.error.error.message) {
+      switch (error.error.error.message) {
+        case 'EMAIL_EXISTS':
+          this.erro = 'O e-mail já está em uso.';
+          break;
+        case 'INVALID_LOGIN_CREDENTIALS':
+          this.erro = 'A senha inserida é inválida.';
+          break;
+        default:
+          this.erro = 'Ocorreu um erro ao cadastrar o usuário.';
+          break;
+      }
+      this.temErro = true;
+      this.estaCarregando = false;
+    }
   }
   
 
